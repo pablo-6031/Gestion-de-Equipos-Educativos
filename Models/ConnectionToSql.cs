d1fd55c890bc01213a15d0e97b4c75161ccabd75
Pablo Carrizo <pablo6031@gmail.com>
2024-10-28 20:34:18 -0300

    Interfaz Basica de Login

---
+++ Models/ConnectionToSql.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Models
+{
+    public class ConnectionToSql
+    {
+        private readonly string connectionString;
+        public ConnectionToSql()
+        {
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename= C:\\Users\\pablo\\source\\repos\\Gestion de Equipos Educativos\\DataBase\\DB_GestionEquipos.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+    }
+}
